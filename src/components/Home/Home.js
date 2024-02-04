29dc34328fa615b872d036f1d87a65c4ff956db8
Gurjeet Singh <gurjeet21walia@gmail.com>
2024-02-04 23:34:36 +0530

    changes

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,17 +1,21 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
+import Services from "./Services";
 
 function Home() {
   return (
     <section>
       <Container fluid className="home-section" id="home">
-        <Container className="home-content">
+        <Container fluid className="home-content">
           <Row>
             <Col md={7} className="home-header">
-              <h1 style={{ paddingBottom: 15 }} className="heading">
-           
-              </h1>
-
+              <h2 className="title">
+                Join our Community
+              </h2>
+              <p>Our community of creators is awaiting you!</p>
+              <p>GlobalNation™ is about supporting the creators whose work inspires you, and to become a creator and inspire others.</p>
+              <p>Join us to Create, Connect with other creators, and Collect their work in support of them.</p>
+              <a className="join-btn" href="#">Join the Beta</a>
             </Col>
           </Row>
         </Container>
